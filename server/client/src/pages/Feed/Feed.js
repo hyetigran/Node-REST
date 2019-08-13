c0c5c6fb522cc07bccf16931f3fc2a8ba5c00b84
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-13 21:11:02 +0400

    add: socket io to delete and update handlers on front and back end

--- server/client/src/pages/Feed/Feed.js
+++ server/client/src/pages/Feed/Feed.js
@@ -40,6 +40,10 @@ class Feed extends Component {
     socket.on('posts', data => {
       if (data.action === 'create') {
         this.addPost(data.post)
+      } else if (data.action === 'update') {
+        this.updatePost(data.post)
+      } else if (data.action === 'delete') {
+        this.loadPosts()
       }
     })
   }
@@ -48,12 +52,27 @@ class Feed extends Component {
     this.setState(prevState => {
       const updatedPosts = [...prevState.posts];
       if(prevState.postPage === 1) {
-        updatedPosts.pop();
+        if(prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
         updatedPosts.unshift(post);
       }
       return {
         posts: updatedPosts,
         totalPosts: prevState.totalPosts + 1
+      }
+    })
+  }
+
+  updatePost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(p => p._id === post._id);
+      if(updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts
       }
     })
   }
@@ -169,17 +188,7 @@ class Feed extends Component {
           createdAt: resData.post.createdAt
         };
         this.setState(prevState => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              p => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
-          }
           return {
-            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
@@ -217,10 +226,11 @@ class Feed extends Component {
       })
       .then(resData => {
         console.log(resData);
-        this.setState(prevState => {
-          const updatedPosts = prevState.posts.filter(p => p._id !== postId);
-          return { posts: updatedPosts, postsLoading: false };
-        });
+        this.loadPosts();
+        // this.setState(prevState => {
+        //   const updatedPosts = prevState.posts.filter(p => p._id !== postId);
+        //   return { posts: updatedPosts, postsLoading: false };
+        // });
       })
       .catch(err => {
         console.log(err);
--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -16,6 +16,7 @@ exports.getPosts = (req, res, next) => {
       totalItems = count;
       return Post.find()
         .populate("creator")
+        .sort({ createdAt: -1 })
         .skip((currentPage - 1) * perPage)
         .limit(perPage);
     })
@@ -68,7 +69,10 @@ exports.createPost = (req, res, next) => {
       return user.save();
     })
     .then(result => {
-      io.getIO().emit("posts"), { action: "create", post: post };
+      io.getIO().emit("posts", {
+        action: "create",
+        post: { ...post._doc, creator: { _id: req.userId, name: user.name } }
+      });
       res.status(201).json({
         message: "Post created successfully!",
         post: post,
@@ -110,7 +114,7 @@ exports.updatePost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
-  const { title, conetent } = req.body;
+  const { title, content } = req.body;
   let imageUrl = req.body.image;
   if (req.file) {
     imageUrl = req.file.path;
@@ -121,13 +125,14 @@ exports.updatePost = (req, res, next) => {
     throw error;
   }
   Post.findById(postId)
+    .populate("creator")
     .then(post => {
       if (!post) {
         const error = new Error("Could not find post.");
         error.statusCode = 404;
         throw error;
       }
-      if (post.creator.toString() !== req.userId) {
+      if (post.creator._id.toString() !== req.userId) {
         const error = new Error("Not authorized");
         error.statusCode = 403;
         throw error;
@@ -141,6 +146,7 @@ exports.updatePost = (req, res, next) => {
       return post.save();
     })
     .then(result => {
+      io.getIO().emit("posts", { action: "update", post: result });
       res.status(200).json({ message: "Post updated", post: result });
     })
     .catch(err => {
@@ -176,6 +182,7 @@ exports.deletePost = (req, res, next) => {
       return user.save();
     })
     .then(result => {
+      io.getIO().emit("posts", { action: "delete", post: postId });
       res.status(200).json({ message: "Deleted post" });
     })
     .catch(err => {
